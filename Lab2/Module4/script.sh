a7d76e0223c74a4a902878680088cfee7b5772c9
sideshowbobgot <sideshowbobgot@gmail.com>
2024-03-31 20:09:19 +0300

    added Module4 for lab2

---
+++ Lab2/Module4/script.sh
@@ -0,0 +1,36 @@
+First Row:
+	Column names:
+		Last name: Text is left aligned and bold.
+		First name: Text is left aligned and bold.
+		Phone: Text is left aligned and bold.
+		Email: Text is left aligned and bold.
+		Admin: Text is left aligned and bold.
+Second Row:
+	This a single cell that takes up the whole row.
+	There is a reference with text "Add Contact".
+	The reference directs to google.com under the hood
+	Text is right aligned.
+Third Row
+	Last name: Doe. Text is left aligned.
+	First name: Jane. Text is left aligned.
+	Phone: 010-110-1101. Text is left aligned.
+	Email: janed@someotheraddress.org. Text is left aligned. Must look like URL, it directs to google.com. URL Must not have underlining.
+	Admin:
+		This cell must be splitted into two parts.
+		The first part is a reference with text "Edit".
+		The second part is a reference with text "Remove".
+		There must be a border between edit and remove cells.
+		They both direct to google.com
+Fourth Row
+	Last name: Johnson. Text is left aligned.
+	First name: Roberto. Text is left aligned.
+	Phone: 123-456-7890. Text is left aligned.
+	Email: robertoj@someaddress.com. Text is left aligned. Must look like URL, it directs to google.com. URL Must not have underlining.
+	Admin:
+		This cell must be splitted into two parts.
+		The first part is a reference with text "Edit".
+		The second part is a reference with text "Remove".
+		There must be a border between edit and remove cells.
+		They both direct to google.com
+
+Table must have thin black borders. Do not use CSS. Use as much Html capabilities as possible. 
